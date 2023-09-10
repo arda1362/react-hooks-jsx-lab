674e68eb9aead6275e149956d5bc4df577984e0a
Ardavan Zareirad <ardavan2525@gmail.com>
2023-09-10 13:12:08 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { image } from "../data/data";
+import React from 'react';
+import { image } from '../data/data'; // Import image
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>this is Ardavan.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
